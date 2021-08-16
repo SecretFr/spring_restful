5d0ae35d8eaeb9d4d6ec76f9a00d91710f869d80
SecretFr <linkclean@naver.com>
2021-08-17 05:08:32 +0900

    use jpa / h2-console

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'test1', '123asd', '910999-1403823')
+insert into user values (2, sysdate(), 'test2', '123sdfffasd', '910999-1403823')
+insert into user values (3, sysdate(), 'test3', '123asdfsd', '910999-1403823')
