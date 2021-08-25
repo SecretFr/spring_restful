66f59b6f5d39c50ba0d9a71dcb22b67d5d8a950a
SecretFr <linkclean@naver.com>
2021-08-25 22:47:27 +0900

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into user values (1, sysdate(), 'test1', '123asd', '910999-1403823')
-insert into user values (2, sysdate(), 'test2', '123sdfffasd', '910999-1403823')
-insert into user values (3, sysdate(), 'test3', '123asdfsd', '910999-1403823')
+insert into user values (9001, sysdate(), 'user1', '1234', '910999-1403823');
+insert into user values (9002, sysdate(), 'user2', '1234', '910999-1403823');
+insert into user values (9003, sysdate(), 'user3', '1234', '910999-1403823');
+
+insert into post values (1001, 'My first post', 9001);
+insert into post values (1002, 'My second post', 9001);
